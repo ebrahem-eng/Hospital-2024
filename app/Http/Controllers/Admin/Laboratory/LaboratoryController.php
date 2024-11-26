885606ef19f3a433c7fe9495e79e31105605d091
Ebrahem Alwish <ebrahemk968@gmail.com>
2024-11-26 12:29:01 +0300

    Admin Manage Laboratory

---
+++ app/Http/Controllers/Admin/Laboratory/LaboratoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Laboratory;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LaboratoryController extends Controller
+{
+    //
+}
